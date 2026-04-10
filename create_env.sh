a4241b472dbd805c6ce3b2cb2cdea1c48c46b60e
Mikey Ferguson <mikeyferguson49@icloud.com>
2026-04-10 18:12:42 -0500

    setuptools

--- create_env.sh
+++ create_env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash                                  
-eval "$(conda shell.bash hook)"                                                                                         
+eval "$(conda shell.bash hook)" 
+conda env remove -n amazon-m2-venv -y                                                                                        
 conda create -n amazon-m2-venv python=3.10 -y
 conda activate amazon-m2-venv
 pip install torch --index-url https://download.pytorch.org/whl/cu118
-pip install recbole "ray[tune]" kmeans-pytorch "numpy<2.0"   
+pip install setuptools
+pip install recbole "ray[tune]" kmeans-pytorch "numpy<2.0" 
