afe74b4d0aabe9ec3a939165eb294c0b7961234d
Mikey Ferguson <mikeyferguson49@icloud.com>
2026-04-15 11:32:58 -0500

    create_env update

--- create_env.sh
+++ create_env.sh
@@ -1,8 +1,12 @@
-#!/bin/bash                                  
+#!/bin/bash
+set -e
+module purge
+module load Anaconda3/2025.06-0                            
 eval "$(conda shell.bash hook)" 
-conda env remove -n amazon-m2-venv -y                                                                                        
+conda env remove -n amazon-m2-venv -y || true                                                                                     
 conda create -n amazon-m2-venv python=3.10 -y
 conda activate amazon-m2-venv
 pip install torch --index-url https://download.pytorch.org/whl/cu118
 pip install setuptools
 pip install recbole "ray[tune]" kmeans-pytorch "numpy<2.0" 
+python -c "import recbole; import torch; print('ok', recbole.__file__, torch.__version__)"
