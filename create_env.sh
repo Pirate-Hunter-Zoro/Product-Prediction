1dfe250944a1d0fc3532d10bd49807cf1a5ba851
Mikey Ferguson <mikeyferguson49@icloud.com>
2026-04-15 14:01:18 -0500

    env creation

--- create_env.sh
+++ create_env.sh
@@ -7,6 +7,6 @@ conda env remove -n amazon-m2-venv -y || true
 conda create -n amazon-m2-venv python=3.10 -y
 conda activate amazon-m2-venv
 PYTHONNOUSERSITE=1 python -m pip install --no-user torch --index-url https://download.pytorch.org/whl/cu118
-PYTHONNOUSERSITE=1 python -m pip install --no-user setuptools
+PYTHONNOUSERSITE=1 python -m pip install --no-user "setuptools<80"
 PYTHONNOUSERSITE=1 python -m pip install --no-user recbole "ray[tune]" kmeans-pytorch "numpy<2.0" 
-PYTHONNOUSERSITE=1 python -c "import recbole; import torch; print(recbole.__file__)"
+PYTHONNOUSERSITE=1 python -c "import recbole; import setuptools; import torch; from recbole.quick_start import run_recbole; print(recbole.__file__); print(setuptools.__version__)"
