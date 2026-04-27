4d4bdd0b9b883873bc39d48760e2bd8528b065db
Mikey Ferguson <mikeyferguson49@icloud.com>
2026-04-27 15:23:41 -0500

    new model implemented

--- scripts/attribute_loader.py
+++ scripts/attribute_loader.py
@@ -26,9 +26,19 @@ def load_text_embedding(
     Returns:
         torch.Tensor: Shape (num_items, embedding_dim); Row 0 is all zeros while rows i > 0 hold attribute embeddings for items of corresponding id=i
     """
+    # Load item ALL IDS and see which ones are present in the given dataset
     pickle_path = Path(pickle_path) # In case of str
     payload = torch.load(pickle_path)
     ext_ids, embeddings = payload['item_ids'], payload['embeddings']
+    known_tokens = set(dataset.field2token_id[item_id_field])
+    present = [1 if id in known_tokens else 0 for id in ext_ids]
+    n_total = len(ext_ids)
+    n_missing = n_total - sum(present)
+    if n_missing > 0:
+        print(f"Missing {n_missing} out of {n_total} items in the input dataset...", flush=True)
+    ext_ids = [ext_ids[i] for i in range(n_total) if present[i]]
+    embeddings = embeddings[torch.as_tensor(present, dtype=torch.bool)]
+    
     embed_dim = embeddings.shape[1] # Embedding dimension
     num_items = dataset.num(item_id_field)
     output = torch.zeros(num_items, embed_dim, dtype=embeddings.dtype)
@@ -55,15 +65,26 @@ def load_price_bins(
     Returns:
         torch.Tensor: Shape (num_items, ) Row 0's value is n_bins to be used as a reserved sentinel, rows 1..num_items-1 are the bin indices of each respective item
     """
+    # Load in ids and price bins and filter out missing items
     pickle_path = Path(pickle_path)
     payload = torch.load(pickle_path)
-    external_ids, bins = payload['item_ids'], payload['bin_idx']
+    ext_ids, bins = payload['item_ids'], payload['bin_idx']
+    known_tokens = set(dataset.field2token_id[item_id_field])
+    present = [1 if id in known_tokens else 0 for id in ext_ids]
+    n_total = len(ext_ids)
+    n_missing = n_total - sum(present)
+    if n_missing > 0:
+        print(f"Missing {n_missing} out of {n_total} items in the input dataset...", flush=True)
+    ext_ids = [ext_ids[i] for i in range(n_total) if present[i]]
+    bins = bins[torch.as_tensor(present, dtype=torch.bool)]
+    
     assert bins.dtype == torch.long, f"Expected bins dtype of {torch.long} but received type {bins.dtype}"
     num_items = dataset.num(item_id_field)
+    
     # Every row should start at the value 'n_bins', and will be overrided later
     output = torch.full((num_items,), n_bins, dtype=torch.long) # This way items who for some reason don't have a price bin associated with them will be thrown into bin 'n_bins' (though we designed this pipeline to not let that happen)
     # Bind each item to its internal ID
-    internal_ids = torch.as_tensor(dataset.token2id(item_id_field, external_ids), dtype=torch.long)
+    internal_ids = torch.as_tensor(dataset.token2id(item_id_field, ext_ids), dtype=torch.long)
     # Each item will be assigned its corresponding price bin
     output[internal_ids] = bins
     return output
--- scripts/train.py
+++ scripts/train.py
@@ -1,23 +1,37 @@
 from recbole.quick_start import run_recbole
+import recbole.quick_start.quick_start as quick_start
 import argparse
 import torch
 
-_original_load = torch.load
+from novel_model import NovelModel
 
+# Monkey patch the torch.load function
+_original_load = torch.load
 def _patched_load(*args, **kwargs):
     kwargs.setdefault("weights_only", False)
     return _original_load(*args, **kwargs)
-
 torch.load = _patched_load
+
+# Monkey patch the quick_start.get_model function
+_original_get_model = quick_start.get_model
+def _patched_get_model(model_name):
+    if model_name == "NovelModel":
+        return NovelModel
+    return _original_get_model(model_name)
+quick_start.get_model = _patched_get_model
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, required=True)
     args = parser.parse_args()
     
+    # Figure out what model we will be using - Recbole is weird so for our novel model we need a direct reference to its class
+    # Due to monkey patch of loading model above, this should be redundant but harmless
+    model_arg = NovelModel if args.model == "NovelModel" else args.model
+    
     print("Running recbole training...", flush=True)
     run_recbole(
-        model=args.model,
+        model=model_arg,
         dataset='amazon_m2',
         config_file_list=['scripts/config.yaml']
     )
